f542d916aa38d96fb5c299712018867239741832
Mateusz Nieścior <mateuszniescior@Mateuszs-MacBook-Pro.local>
2018-06-14 07:05:42 +0200

    First commit

---
+++ back-end/src/main/java/com/social/services/UserService.java
@@ -0,0 +1,39 @@
+package com.social.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.social.dao.UserRepository;
+import com.social.entities.User;
+/** 
+ * @author kamal berriga
+ *
+ */
+@Service
+public class UserService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	public User save(User user) {
+
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		return userRepository.saveAndFlush(user);
+	}
+
+	public User update(User user) {
+		return userRepository.save(user);
+	}
+
+	public User find(String userName) {
+		return userRepository.findOneByUsername(userName);
+	}
+
+	public User find(Long id) {
+		return userRepository.findOne(id);
+	}
+}
