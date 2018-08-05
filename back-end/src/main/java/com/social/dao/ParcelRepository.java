fd041dfd68c53f6340135f3415ba98b1d1d62f04
lumeen <mateusz.niescior@gmail.com>
2018-08-05 22:38:57 +0200

    Update ParcelRepository.java

--- back-end/src/main/java/com/social/dao/ParcelRepository.java
+++ back-end/src/main/java/com/social/dao/ParcelRepository.java
@@ -7,10 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ParcelRepository extends JpaRepository<Parcel, Long> {
 
     List<Parcel> findByIdIn(List<Long> id);
-    List<Parcel> findByCarId(Long carId);
     List<Parcel> findById(Long id);
     List<Parcel> findByStartPhoneAndEndPhoneAndStatus(String startPhone, String ednPhone, String status);
     List<Parcel> findByStartPhoneAndStatus(String startPhone, String status);
+        List<Parcel> findByCarId(Long carId);
     List<Parcel> findByEndPhoneAndStatus(String endPhone, String status);
 List<Parcel> findByCourierId(Long courierId);
 
